51c8f905a8b73d4bc16cda27fcf815ff6977b455
Sargam Tripathi <sargam688@gmail.com>
2026-07-16 23:27:42 +0530

    Add: kth-largest-integer-in-a-stream - submission-0

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-0.cpp
@@ -0,0 +1,26 @@
+class KthLargest {
+
+    private:
+    int k;
+    priority_queue<int,vector<int>,greater<int>>min_heap;
+
+public:
+    KthLargest(int k, vector<int>& nums) {
+
+        this->k=k;
+        for(int num:nums){
+            add(num);
+        }
+        
+    }
+    
+    int add(int val) {
+        min_heap.push(val);
+        if(min_heap.size()>k){
+            min_heap.pop();
+        }
+
+        return min_heap.top();
+        
+    }
+};
